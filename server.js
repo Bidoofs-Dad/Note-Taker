ad970d56749f1ca4eaa0109f0949b6b13adad012
Zane Werle <zanewerle@gmail.com>
2023-05-16 12:10:50 -0500

    added in the get and post routes for the api, just have to make it so when you post, it actually saves the data in the json

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const path = require("path");
+const notesData = require("./db/db.json")
 
 const PORT = 3001;
 const app = express();
@@ -11,6 +13,18 @@ app.use(express.urlencoded({extended: true}));
 app.get("/notes", (req, res) => {
     res.sendFile(__dirname + "/public/notes.html")
 })
+
+app.get("/api/notes", (req, res) => {
+    res.json(notesData)
+})
+
+app.post("/api/notes", (req, res) => {
+    console.log("body", req.body);
+    console.log("req.method", req.method);
+    res.json("TESTTTT")
+})
+
+
 app.get("*", (req, res) => {
     res.sendFile(__dirname + "/public/index.html")
 })
