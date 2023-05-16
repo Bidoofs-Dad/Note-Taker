d469e970b3667366c89f7b82b9d3937dbbbdfd96
Zane Werle <zanewerle@gmail.com>
2023-05-16 11:52:11 -0500

    added in the get routes for notes.html and made it so * brings you to the index.html

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-
+app.get("/notes", (req, res) => {
+    res.sendFile(__dirname + "/public/notes.html")
+})
+app.get("*", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html")
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on http://localhost:${PORT}`);
