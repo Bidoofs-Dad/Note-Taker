8238225c9aac59f19bb38430e9eb09a40d7a1593
Zane Werle <zanewerle@gmail.com>
2023-05-16 13:35:53 -0500

    made it so the POST actually works, it properlly logs the data into the json file and appears on the left of the page (only after refreshing though)

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("fs");
 const path = require("path");
 const notesData = require("./db/db.json")
 
@@ -8,7 +9,7 @@ const app = express();
 app.use(express.static("public"));
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/notes", (req, res) => {
     res.sendFile(__dirname + "/public/notes.html")
@@ -18,11 +19,34 @@ app.get("/api/notes", (req, res) => {
     res.json(notesData)
 })
 
-app.post("/api/notes", (req, res) => {
-    console.log("body", req.body);
-    console.log("req.method", req.method);
-    res.json("TESTTTT")
-})
+app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+
+    const newData = {
+        title,
+        text
+    };
+
+    let jsonData = [];
+
+    fs.promises.readFile(path.join(__dirname, "db", "db.json"), "utf-8")
+        .then(fileData => {
+            jsonData = JSON.parse(fileData);
+
+            jsonData.push(newData);
+
+            return fs.promises.writeFile(path.join(__dirname, "db", "db.json"), JSON.stringify(jsonData));
+        })
+        .then(() => {
+            console.log('Data saved successfully.');
+            res.status(200).json(jsonData);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            res.status(500).json({ message: 'Failed to save data.' });
+        });
+});
+
 
 
 app.get("*", (req, res) => {
